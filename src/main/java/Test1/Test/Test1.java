26f1f106f79fce4c881ba71c3ba55c871731aeef
SachinNalawade9 <sachinnalawade36@gmail.com>
2024-04-23 02:11:46 +0530

    Testcase3

---
+++ src/main/java/Test1/Test/Test1.java
@@ -0,0 +1,17 @@
+package Test1.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Test Case 2");
+        
+        System.out.println("Test Case 3");
+    }
+}
